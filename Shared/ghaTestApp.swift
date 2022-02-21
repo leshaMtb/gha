8cab42ada0a1d012eb4ce00e2a2463854e1d8a73
Aleksey <aleksey.shirokov@sravni.ru>
2022-02-21 16:24:50 +0300

    Initial Commit

---
+++ Shared/ghaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ghaTestApp.swift
+//  Shared
+//
+//  Created by Aleksey Shirokov on 21.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ghaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
